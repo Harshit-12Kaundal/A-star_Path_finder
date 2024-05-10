953922e779f5fae023d2e791841105308ba4bdc5
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2024-05-10 19:57:28 +0530

    completed A-star algo

--- main.py
+++ main.py
@@ -102,7 +102,41 @@ def algorithm(draw ,grid, start,end):
     open_set.put((0,count,start))
     came_from={}
     g_score ={spot:float("inf") for row in grid for spot in row}
+    g_score[start]=0
+    f_score ={spot:float("inf") for row in grid for spot in row}
+    f_score[start]=h(start.get_pos(),end.get_pos())
     
+    open_set_hash={start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        current =open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end: 
+            return True
+        
+        for neighbour in current.neighbors:
+            temp_g_score =g_score[current]+1
+
+            if temp_g_score <g_score[neighbour]:
+                came_from[neighbour]=current
+                g_score[current]=temp_g_score
+                f_score[neighbour]= temp_g_score + h(neighbour.get_pos(), end.get_pos())
+                if neighbour not in open_set_hash:
+                    count+=1
+                    open_set.put((f_score[neighbour] ,count ,neighbour))
+                    open_set_hash.add(neighbour)
+                    neighbour.make_open()
+
+        draw()
+
+        if current !=start:
+            current.make_closed()
+    return False
 
 def make_grid(rows,width):
     grid =[]
